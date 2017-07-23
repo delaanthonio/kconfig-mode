824fd05ac42e93bcca806cdad2463f247ed16abd
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-23 11:02:14 -0400

    kconfig-mode: Refactor regex expressions
    Use rx for more idiomatic regex expressions.

--- kconfig-mode.el
+++ kconfig-mode.el
@@ -34,40 +34,83 @@
 
 ;;; Code:
 
-(defvar kconfig-mode-font-lock-keywords
-  '(("\#.*" . font-lock-comment-face)
+(require 'rx)
 
-    ("\".*\"" . font-lock-string-face)
+(defgroup kconfig nil
+  "Kconfig major mode"
+  :group 'languages)
 
-    ("^[\t, ]*\\_<bool\\_>" . font-lock-type-face)
-    ("^[\t, ]*\\_<int\\_>" . font-lock-type-face)
-    ("^[\t, ]*\\_<boolean\\_>" . font-lock-type-face)
-    ("^[\t, ]*\\_<hex\\_>" . font-lock-type-face)
-    ("^[\t, ]*\\_<tristate\\_>" . font-lock-type-face)
-    ("^[\t, ]*\\_<string\\_>" . font-lock-type-face)
+(defconst kconfig-mode-syntax-table
+  (let ((syntax-table (make-syntax-table)))
 
-    ("^[\t, ]*\\_<def_bool\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<depends on\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<select\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<help\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<---help---\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<default\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<prompt\\_>" . font-lock-variable-name-face)
-    ("^[\t, ]*\\_<range\\_>" . font-lock-variable-name-face)
+    ;; python style comment: “# …”
+    (modify-syntax-entry ?# "<" syntax-table)
+    (modify-syntax-entry ?\n ">" syntax-table)
 
-    ("\\s-[1-9]+\\s-" . font-lock-constant-face)
-    ("\\s-[yn]\\s-" . font-lock-constant-face)
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" syntax-table)
 
-    ("^\\_<choice\\_>" . font-lock-keyword-face)
-    ("^\\_<endchoice\\_>" . font-lock-keyword-face)
-    ("^\\_<config\\_>" . font-lock-keyword-face)
-    ("^\\_<comment\\_>" . font-lock-keyword-face)
-    ("^\\_<menu\\_>" . font-lock-keyword-face)
-    ("^\\_<endmenu\\_>" . font-lock-keyword-face)
-    ("^\\_<if\\_>" . font-lock-keyword-face)
-    ("^\\_<endif\\_>" . font-lock-keyword-face)
-    ("^\\_<menuconfig\\_>" . font-lock-keyword-face)
-    ("^\\_<source\\_>" . font-lock-keyword-face)))
+    ;; `_' as being a valid part of a word
+    (modify-syntax-entry ?_ "w" syntax-table)
+    syntax-table)
+  "Syntax table for `kconfig-mode'.")
+
+(defconst kconfig-type-keywords
+  '("bool" "boolean" "hex" "int" "string" "tristate"))
+
+(defconst kconfig-config-keywords
+  '("def_bool" "default" "depends on" "prompt" "select" "range"))
+
+(defconst kconfig-defun-keywords
+  '("config"))
+
+(defconst kconfig-doc-keywords
+  '("---help---" "help")
+  ;; "Keywords before a description"
+  )
+
+(defconst kconfig-top-level-keywords
+  '("choice" "endchoice" "comment" "if" "endif" "menu" "endmenu" "menuconfig" "source"))
+
+(defconst kconfig-keywords
+  (append kconfig-top-level-keywords kconfig-config-keywords kconfig-defun-keywords kconfig-doc-keywords))
+
+(defconst kconfig-mode-font-lock-defaults
+  `(
+    ;; Documentation
+    (,(rx-to-string
+       `(and
+             line-start space space (+ space) (group(+ not-newline)) line-end
+             )
+       t)
+     1 font-lock-doc-face t)
+    ;; keywords
+    (,(rx-to-string
+       `(and  symbol-start (group (or ,@kconfig-keywords)) symbol-end)
+       t)
+     1 font-lock-keyword-face)
+    ;; types
+    (,(rx-to-string
+       `(and word-start (group (or ,@kconfig-type-keywords)) word-end)
+       t)
+     1 font-lock-type-face)
+    ;; constants
+    (,(rx-to-string
+       `(and word-start (group (or "y" "n")) word-end)
+       t)
+     1 font-lock-constant-face)
+     (,(rx-to-string
+        `(and word-start "default" word-end (+ space)
+              word-start (group (\? "0x") (+ hex)) word-end)
+        t)
+     1 font-lock-constant-face)
+    ;; Config declarations
+    (,(rx-to-string
+       `(and line-start word-start (or ,@kconfig-defun-keywords) word-end
+             (+ space) word-start (group (+ (or alnum "_"))) word-end)
+       t)
+     1 font-lock-function-name-face t)
+    )   "Font lock faces highlighting for `kconfig-mode'.")
 
 (defvar kconfig-headings
   '("bool" "int" "boolean" "hex" "tristate" "string" "def_bool" "depends on"
@@ -93,18 +136,22 @@
 (defconst kconfig-imenu-generic-expression
   '(("Config" "^config *\\(.+\\)" 1)))
 
-(define-derived-mode kconfig-mode text-mode "Kconfig"
+(define-derived-mode kconfig-mode prog-mode "Kconfig"
   "Major mode for editing Kconfig files in the Linux kernel."
-  (setq-local comment-start "# ")
+  (setq-local comment-start "#")
+  (setq-local comment-padding 1)
   (setq-local comment-end "")
-  (setq-local font-lock-defaults '(kconfig-mode-font-lock-keywords t))
+  (setq-local font-lock-defaults '((kconfig-mode-font-lock-defaults) nil nil))
   (setq-local outline-regexp (concat "^[\t ]*" (regexp-opt kconfig-headings)))
   (setq-local outline-level 'kconfig-outline-level)
   (setq-local imenu-generic-expression kconfig-imenu-generic-expression)
-  (add-hook 'completion-at-point-functions 'kconfig-completion-at-point nil 'local))
+  (add-hook 'completion-at-point-functions 'kconfig-completion-at-point nil 'local)
+
+  :group 'kconfig
+  :syntax-table kconfig-mode-syntax-table)
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("Kconfig" . kconfig-mode))
+(add-to-list 'auto-mode-alist '("\\Kconfig\\" . kconfig-mode))
 
 (provide 'kconfig-mode)
 ;;; kconfig-mode.el ends here
